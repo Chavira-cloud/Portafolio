b114c61aa0010d4e88cc581a9faa8bef39e72980
Chavira-cloud <markos.mcgc@gmail.com>
2025-05-01 23:52:02 -0700

    Avance math js

--- Javascript/math.js
+++ Javascript/math.js
@@ -1,29 +1,95 @@
-console.group('Circulo');
+console.group('Cuadrado') 
 
-const radioCirculo = 3;
-const diametroCirculo = radioCirculo * 2;
-const PI = 3.1416
-
-const circuferencia = diametroCirculo * PI;
-const areaCirculo = (radioCirculo ** 2) * PI;
+const ladoCuadrado = 5;
+const perimetroCuadrado = ladoCuadrado * 4;
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
 
 console.log({
-    radioCirculo,
-    diametroCirculo,
-    PI,
-    circuferencia,
-    areaCirculo
+  ladoCuadrado,
+  perimetroCuadrado,
+  areaCuadrado,
 });
 
-function calcularCirculo(radio){
-    const diametro= radio * 2;
-    const radioAlCuadrado = Math.pow(radio, 2)
+function calcularCuadrado(lado) {
+  return {
+    perimetro: lado * 4,
+    area: lado * lado,
+  };
+}
 
-    return{
-        circuferencia: diametro * Math.PI,
-        area: radioAlCuadrado * Math.PI,
-    };
+console.groupEnd('Cuadrado') 
+
+console.group('Triangulo')
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const ladoTrianguloBase = 4;
+const alturaTriangulo = 5.5;
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguloBase;
+const areaTriangulo = (ladoTrianguloBase * alturaTriangulo) / 2;
+
+function calcularTriangulo(lado1, lado2, base, altura) {
+  return {
+    perimetro: lado1 + lado2 + base,
+    area: (base * altura) / 2,
+  };
 }
 
 
-console.groupEnd('Circulo');
+function calcularAlturaTriangulo(lado1, base) {
+  if (lado1 == base) {
+    console.warn('Este no es un triángulo isosceles');
+  } else {
+    // h = raizcuadrada(lado1**2 - (b**2)/4)
+    return Math.sqrt( (lado1 ** 2) - ( (base ** 2) ) / 4 );
+  }
+}
+
+/* function calcularAlturaTrianguloIsoceles(lado1,lado2,lado3, base) {
+    // h = raizcuadrada(lado1**2 - (b**2)/4)
+    const s = (lado1 + lado2 + lado3) / 2;
+    const area = Math.sqrt(s * (s-lado1) * (s-lado2) * (s-lado3));
+    const altura = (2* area) / base;
+    return altura.toFixed(2);
+    } */
+
+console.log({
+  ladoTriangulo1,
+  ladoTriangulo2,
+  ladoTrianguloBase,
+  alturaTriangulo,
+  perimetroTriangulo,
+  areaTriangulo,
+});
+
+console.groupEnd('Triangulo') 
+
+console.group('Circle')
+
+const radioCirculo = 3;
+const diametroCirculo = radioCirculo * 2;
+const PI = 3.1415;
+
+const circunferencia = diametroCirculo * PI;
+const areaCirculo = (radioCirculo ** 2) * PI;
+
+console.log({
+  radioCirculo,
+  diametroCirculo,
+  PI,
+  circunferencia,
+  areaCirculo,
+});
+
+function calcularCirculo(radio) {
+  const diametro = radio * 2;
+  const radioAlCuadrado = Math.pow(radio, 2);
+  
+  return {
+    circunferencia: diametro * Math.PI,
+    area: radioAlCuadrado * Math.PI,
+  };
+}
+
+console.groupEnd('Circle')
